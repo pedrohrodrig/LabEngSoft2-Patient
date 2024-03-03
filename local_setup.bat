0c607c1ad8e45af521655736017255da26f51b04
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-03 02:07:22 -0300

    Altera script

--- local_setup.bat
+++ local_setup.bat
@@ -9,5 +9,7 @@ pause
 mkdir backend
 cd backend
 py -m pip install pipenv
-pipenv install --dev --python 3.10
+pipenv shell --python 3.10
 pipenv run pre-commit install
+pipenv install django
+pipenv install djangorestframework
