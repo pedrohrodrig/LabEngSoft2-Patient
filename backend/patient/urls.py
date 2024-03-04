3322680a3f2f2c428d22e604c88659b8e8021e60
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-03 21:32:24 -0300

    Adiciona endpoint de paciente especifico

--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from .views import (
-    PatientView
-)
+from .views import PatientView
 
 urlpatterns = [
-    path(
-        "patient/",
-        PatientView.as_view(actions={"post": "create_patient"})
-    )
+    path("patient/", PatientView.as_view(actions={"post": "create"})),
+    path("patient/<int:pk>/", PatientView.as_view(actions={"get": "retrieve"})),
 ]
--- backend/patient/views.py
+++ backend/patient/views.py
@@ -5,14 +5,24 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Patient
 from .serializers import PatientSerializer
 
-class PatientView(ModelViewSet):
 
-    def create_patient(self, request):
+class PatientView(ModelViewSet):
+    def create(self, request):
         serializer = PatientSerializer(data=request.data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
         patient = Patient.objects.create(**serializer.validated_data)
         patient_serialized = PatientSerializer(patient)
 
         return Response(patient_serialized.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk):
+        patient = Patient.objects.filter(pk=pk).first()
+
+        if not patient:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PatientSerializer(patient)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
