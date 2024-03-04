865d2e172e549a2e252b2c45d2730fd628a769b7
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-03 21:33:09 -0300

    Adiciona campos do cadastro do cliente e necessários para serviço de autenticação futuramente implementado

--- backend/patient/models.py
+++ backend/patient/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Patient(models.Model):
+    id_user = models.PositiveIntegerField(unique=True)
     full_name = models.CharField(max_length=255, null=False, blank=False)
+    date_of_birth = models.DateField(blank=False, null=False)
     email = models.EmailField(max_length=255, unique=True, blank=False, null=False)
     phone_number = models.CharField(max_length=15, blank=True, default="")
-    document = document = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    document = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    address = models.CharField(max_length=255, null=False, blank=False)
 
     class Meta:
         db_table = "patient"
