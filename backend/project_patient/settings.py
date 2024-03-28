7eb1f8c52cf5ac56915cff537039eeccd12572f1
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-28 00:05:05 -0300

    Configura autenticação via serviço

--- backend/project_patient/settings.py
+++ backend/project_patient/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTTokenUserAuthentication",),
+}
+
+SIMPLE_JWT = {"SIGNING_KEY": "5ahp8kseKOVB_w"}
